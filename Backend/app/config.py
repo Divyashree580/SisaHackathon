59096c2249ee58c32a6bc516f0cab96bec978f48
Divyashree580 <divyashreeb0584@gmail.com>
2026-06-24 22:30:01 +0530

    Added cors

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -26,10 +26,7 @@ class Settings:
     # CORS Configurations
     CORS_ORIGINS: list = [
         "http://localhost:5173",  # React/Vite dev port
-        "http://127.0.0.1:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "*"
+        "http://127.0.0.1:5173"
     ]
 
 settings = Settings()
